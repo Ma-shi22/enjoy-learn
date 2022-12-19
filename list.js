6309d8bafe75d488497d2a1632cdc2ab0bdab1c9
unknown <mashisantos@gmail.com>
2022-12-19 19:04:56 +0100

    new Maria

---
+++ list.js
@@ -0,0 +1,165 @@
+let coderList = [
+    {
+        name: "MARCOS",
+        img: "Archivos/Marciano1.png",
+        id: 0,
+        life: true
+    },
+    {
+        name: "MARGARITA",
+        img: "Archivos/Marciano2.png",
+        id: 1,
+        life: true
+    },
+    {
+        name:"CLAUDETTE",
+        img:"Archivos/Marciano_3.png",
+        id:2,
+        life: true
+    },
+    {
+        name:"MAGDALENA",
+        img:"Archivos/Marciano1.png",
+        id:3,
+        life: true
+    },
+    {
+        name:"ALDARA",
+        img:"Archivos/Marciano2.png",
+        id:4,
+        life: true
+    },
+    {
+        name:"MARY",
+        img:"AArchivos/Marciano_3.png",
+        id:5,
+        life: true
+    },
+    {
+        name:"ROCIO",
+        img:"Archivos/Marciano1.png",
+        id:6,
+        life: true
+    },
+    {
+        name:"CELE",
+        img:"Archivos/Marciano2.png",
+        id:7,
+        life: true
+    },
+    {
+        name:"ROSIMERY",
+        img:"AArchivos/Marciano_3.png",
+        id:8,
+        life: true
+    },
+    {
+        name:"CARMEN",
+        img:"Archivos/Marciano1.png",
+        id:9,
+        life: true
+    },
+    {
+        name:"JENNIFER",
+        img:"Archivos/Marciano2.png",
+        id:10,
+        life: true
+    },
+    {
+        name:"DELIA",
+        img:"Archivos/Marciano_3.png",
+        id:11,
+        life: true
+    },
+    {
+        name:"NOHE",
+        img:"Archivos/Marciano1.png",
+        id:12,
+        life: true
+    },
+    {
+        name:"EVA",
+        img:"Archivos/Marciano2.png",
+        id:13,
+        life: true
+    },
+    {
+        name:"AINHOA",
+        img:"Archivos/Marciano_3.png",
+        id:14,
+        life: true
+    },
+    {
+        name:"MIRIAM",
+        img:"Archivos/Marciano1.png",
+        id:15,
+        life: true
+    },
+    {
+        name:"MILA",
+        img:"Archivos/Marciano2.png",
+        id:16,
+        life: true
+    },
+    {
+        name:"MARTA",
+        img:"Archivos/Marciano_3.png",
+        id:17,
+        life: true
+    },
+    {
+        name:"MARIA",
+        img:"Archivos/Marciano1.png",
+        id:18,
+        life: true
+    },
+    {
+        name:"ROSIBEL",
+        img:"Archivos/Marciano2.png",
+        id:19,
+        life: true
+    },
+    {
+        name:"ANYI",
+        img:"Archivos/Marciano_3.png",
+        id:20,
+        life: true
+    },
+    {
+        name:"CELIA",
+        img:"Archivos/Marciano1.png",
+        id:21,
+        life: true
+    },
+    {
+        name:"DIANA",
+        img:"Archivos/Marciano2.png",
+        id:22,
+        life: true
+    },
+    {
+        name:"ANA",
+        img:"Archivos/Marciano_3.png",
+        id:23,
+        life: true
+    },{
+        name:"PAOLA",
+        img:"Archivos/Marciano1.png",
+        id:24,
+        life: true
+    },
+    {
+        name:"SERGIO",
+        img:"Archivos/Marciano2.png",
+        id:25,
+        life: true
+    },
+    {
+        name:"MIRIAN",
+        img:"Archivos/Marciano_3.png",
+        id:26,
+        life: true
+    },
+    
+];
+
---
+++ photoArray.js
@@ -0,0 +1,151 @@
+let photoList = [
+    {
+      img: "Archivos/Marciano1.png",
+      id: 0,
+    },
+    {
+      img: "Archivos/Marciano2.png",
+      id: 1,
+    },
+    {
+      img: "Archivos/Marciano_3.png",
+      id: 2,
+    },
+    {
+      img: "Archivos/Marciano1.png",
+      id: 3,
+    },
+    {
+      img: "Archivos/Marciano2.png",
+      id: 4,
+    },
+    {
+      img: "Archivos/Marciano_3.png",
+      id: 5,
+    },
+    {
+      img: "Archivos/Marciano1.png",
+      id: 6,
+    },
+    {
+      img: "Archivos/Marciano2.png",
+      id: 7,
+    },
+    {
+      img: "Archivos/Marciano_3.png",
+      id: 8,
+    },
+    {
+      iimg: "Archivos/Marciano1.png",
+      id: 9,
+    },
+    {
+      img: "Archivos/Marciano2.png",
+      id: 10,
+    },
+    {
+      img: "Archivos/Marciano_3.png",
+      id: 11,
+    },
+    {
+      img: "Archivos/Marciano1.png",
+      id: 12,
+    },
+    {
+      img: "Archivos/Marciano2.png",
+      id: 13,
+    },
+    {
+      img: "Archivos/Marciano_3.png",
+      id: 14,
+    },
+    {
+      img: "Archivos/Marciano1.png",
+      id: 15,
+    },
+    {
+      img: "Archivos/Marciano2.png",
+      id: 16,
+    },
+    {
+      img: "Archivos/Marciano_3.png",
+      id: 17,
+    },
+    {
+      img: "Archivos/Marciano1.png",
+      id: 18,
+    },
+    {
+      img: "Archivos/Marciano2.png",
+      id: 19,
+    },
+    {
+      iimg: "Archivos/Marciano_3.png",
+      id: 20,
+    },
+    {
+      img: "Archivos/Marciano1.png",
+      id: 21,
+    },
+    {
+      img: "Archivos/Marciano2.png",
+      id: 22,
+    },
+    {
+      img: "Archivos/Marciano_3.png",
+      id: 23,
+    },
+    {
+      img: "Archivos/Marciano1.png",
+      id: 24,
+    },
+    {
+      img: "Archivos/Marciano2.png",
+      id: 25,
+    },
+    {
+      img: "Archivos/Marciano_3.png",
+      id: 26,
+    },
+    {
+      img: "Archivos/Marciano1.png",
+      id: 27,
+    },
+    {
+      img: "Archivos/Marciano2.png",
+      id: 27,
+    },
+    {
+      img: "Archivos/Marciano_3.png",
+      id: 28,
+    },
+    {
+      img: "Archivos/Marciano1.png",
+      id: 29,
+    },
+    {
+      img: "Archivos/Marciano2.png",
+      id: 31,
+    },
+    {
+      iimg: "Archivos/Marciano_3.png",
+      id: 32,
+    },
+    {
+      img: "Archivos/Marciano1.png",
+      id: 33,
+    },
+    {
+      img: "Archivos/Marciano2.png",
+      id: 34,
+    },
+    {
+      img: "Archivos/Marciano_3.png",
+      id: 35,
+    },
+    {
+      img: "Archivos/Marciano1.png",
+      id: 36,
+    },
+  ];
+  
